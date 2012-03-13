2ab4269346fa30e1fa0d1bcfb268a85306e7b741
Gabriel Farrell <g@grrawr.com>
2012-03-13 18:32:55 -0400

    Added uri search

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var es = require('es');
 var express = require('express');
+var url = require('url');
 
 var app = express.createServer();
 var port = process.argv[2] || 8168;
@@ -7,6 +8,14 @@ var port = process.argv[2] || 8168;
 app.get('/', function(req, res) {
   es.request({
     path: '/free103/_status',
+    res: res,
+    respond: true
+  });
+});
+
+app.get('/search', function(req, res) {
+  es.request({
+    path: '/free103/_search' + url.parse(req.url).search,
     res: res,
     respond: true
   });
