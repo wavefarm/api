ff4b714b01c5f68b9ab2f9958f8e9568d65b22b9
Gabriel Farrell <g@grrawr.com>
2012-08-07 14:06:10 -0400

    Simplified tests

--- test/test-root.js
+++ test/test-root.js
@@ -2,12 +2,6 @@ var assert = require('assert')
 var request = require('request')
 
 request('http://127.0.0.1:1040', function(err, res, body) {
-  var parsed
-  //console.log(body)
-  try {
-    parsed = JSON.parse(body)
-  } catch (e) {
-    assert(false, 'should be able to parse body')
-  }
+  var parsed = JSON.parse(body)
   assert(parsed.ok)
 })
