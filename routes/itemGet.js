6a8297125e35438d3471f2c12542a0611aee23d2
Gabriel Farrell <g@grrawr.com>
2013-08-14 04:54:18 +0000

    Just 404 for now on "get" errors

--- routes/itemGet.js
+++ routes/itemGet.js
@@ -3,7 +3,7 @@ var url = require('url')
 
 module.exports = function (req, res, next, id) {
   es.get({_type: '_all', _id: id}, function (err, data) {
-    if (err) return next(err)
+    if (err) return next() // 404 on any error
     res.end(JSON.stringify(data._source))
   })
 }
