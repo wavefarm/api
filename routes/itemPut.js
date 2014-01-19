5746561a400bc22abcc202a23d6c43be97ff6bd5
Gabriel Farrell <g@grrawr.com>
2014-01-19 01:42:26 -0500

    Beginning schema usage in put processing.
    Refs #2, wavefarm/admin.wavefarm.org#14, and
    wavefarm/admin.wavefarm.org#16

--- routes/itemPut.js
+++ routes/itemPut.js
@@ -1,15 +1,25 @@
 var es = require('../es')
 var scalpel = require('scalpel')
+var schemas = require('../schemas')
 var stack = require('stack')
 
 module.exports = stack(
   scalpel,
   function (req, res, next) {
-    var item = req.parsedBody
-    if (!item.type) {
-      res.statusCode = 400
-      return res.end('{"message": "Item must have a type"}')
-    }
+    var field, schema, item = req.parsedBody;
+    res.invalid = function (errMessage) {
+      res.statusCode = 422;
+      return res.end('{"error": "'+errMessage+'"}');
+    };
+    if (!item.type) return res.invalid('Item must have a type.');
+    schema = schemas[item.type];
+    if (!schema) return res.invalid('No schema found for that type.');
+    schema.item.forEach(function (fieldName) {
+      field = schema.fields[fieldName];
+      if (field.type && field.type.indexOf('rel') === 0) {
+        // TODO retrieve mains for related items
+      }
+    });
     var id = req.params[0]
     es.index({_type: item.type, _id: id}, req.body, function (err, data) {
       if (err) return next(err)
