ad9204913053d2fbffce0d268c2fbc66e48cba98
Gabriel Farrell <g@grrawr.com>
2014-02-28 19:56:17 -0500

    Refactoring broadcast generation around parseAirtime function. Refs #8

--- genb.js
+++ genb.js
@@ -4,59 +4,64 @@ var RRule = require('rrule').RRule
 var timeRe = /\d+:?\d* *(?:a|p).?m/ig
 
 module.exports = function(show) {
-  var options, times, startDate, rule, i, starts
-  // Generate broadcasts for show
-  if (!show.daytime) return
-  options = {}
+  var rule, i, starts, start, broadcast
+  if (!show.airtime) return
 
-  times = getTimes(show.daytime)
-  if (times.length < 2) return console.log('Bad times', show.oldId, show.daytime)
+  // Generate broadcasts for show
+  rule = parseAirtime(show.airtime)
+  console.log(rule.toString())
+  starts = rule.all()
+  i = 0
+  while (++i < starts.length) {
+    start = starts[i]
+    broadcast = {}
+    // TODO Save start and end as UTC
+    broadcast.start = start.toLocaleString()
+    start.setHours(times[1].hours, times[1].minutes, 0, 0)
+
+    // Set end to next day if we cross midnight
+    if (times[1].hours < times[0].hours) start.setDate(start.getDate() + 1)
+
+    broadcast.end = start.toLocaleString()
+    broadcast.shows = [{id: show.id, main: show.main}]
+    broadcast.description = show.description
+
+    console.log(broadcast)
+    break
+  }
+}
+
+// Returns an rrule with an added end time property
+// Can't create another rrule for end times because they may fall on the
+// following day -- so Wednesdays from 11pm to 2am would actually end on
+// Thursday mornings.
+function parseAirtime (airtime) {
+  var times
+  times = getTimes(show.airtime)
+  if (times.length < 2) return console.log('Bad times', show.oldId, show.airtime)
 
   startDate = show.startDate ? new Date(show.startDate) : new Date()
   // Set time on startDate to start time
   startDate.setHours(times[0].hours, times[0].minutes, 0, 0)
   options.dtstart = startDate
-  // TODO set all show dates to 2015-03-01
-  options.until = new Date('2015-03-01') //new Date(show.endDate)
+  // TODO show endDates should all be 2015-03-14, and default to that for
+  // this program term
+  options.until = new Date('2015-03-14') //new Date(show.endDate)
 
-  // TODO parse out frequency, interval, etc. from daytime
+  options = {}
+
+  // TODO parse out frequency, interval, etc. from airtime
   options.freq = RRule.MONTHLY
 
   //console.log(options)
   rule = new RRule(options)
-  //console.log(rule.all())
-  starts = rule.all()
-  i = 0
-  while (++i < starts.length) {
-    var start, broadcast
-    start = starts[i]
-    broadcast = {}
-    // TODO output local ISO string without tz for start and end
-    broadcast.start = start.toLocaleString()
-    start.setHours(times[1].hours, times[1].minutes, 0, 0)
-    if (times[1].hours < times[0].hours) start.setDate(start.getDate + 1)
-    broadcast.end = start.toLocaleString()
-    //console.log(broadcast)
-  }
+  
 }
 
-if (require.main === module) {
-  // Script run directly; generate broadcasts for all shows
-  var queryBody = {
-    filter: {term: {active: true}},
-    size: 9999
-  }
-  es.search({_type: 'show', }, queryBody, function (err, data) {
-    data.hits.hits.forEach(function (hit) {
-      module.exports(hit._source)
-    })
-  })
-}
-
-function getTimes (daytime) {
+function getTimes (airtime) {
   var ampm, hours, minutes, time, times, timeMatch, timeSplit
   times = []
-  while (timeMatch = timeRe.exec(daytime)) {
+  while (timeMatch = timeRe.exec(airtime)) {
     time = timeMatch[0]
     //console.log(time)
     time = time.replace('.', '')
@@ -79,12 +84,25 @@ function getTimes (daytime) {
     time = {hours: Number(hours), minutes: Number(minutes)}
     //console.log(time)
     times.push(time)
-    //timeMatch = timeRe.exec(daytime)
+    //timeMatch = timeRe.exec(airtime)
   }
-  //times.push(timeRe.exec(daytime)[0]) // end time
+  //times.push(timeRe.exec(airtime)[0]) // end time
   
   // Reset the regex index for the start of next string
   timeRe.lastIndex = 0
 
   return times
+}
+
+if (require.main === module) {
+  // Script run directly; generate broadcasts for all shows
+  var queryBody = {
+    filter: {term: {active: true}},
+    size: 9999
+  }
+  es.search({_type: 'show', }, queryBody, function (err, data) {
+    data.hits.hits.forEach(function (hit) {
+      module.exports(hit._source)
+    })
+  })
 }
