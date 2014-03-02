e15035325ed673e5df5d889a982ef32aac132ac1
Gabriel Farrell <g@grrawr.com>
2014-03-02 13:07:43 -0500

    Added id generator. Refs #8 and #13.

---
+++ genid.js
@@ -0,0 +1,12 @@
+// base32 according to http://www.crockford.com/wrmg/base32.html
+var alphabet = '0123456789abcdefghjkmnpqrstvwxyz';
+
+module.exports = function genid () {
+  var x = 6;
+  var id = '';
+  while (x) {
+    id += alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+    x--;
+  }
+  return id;
+};
