2deee15c673a51f19b7412acc78e986f5f4eb763
Gabriel Farrell <g@grrawr.com>
2014-03-03 23:56:17 -0500

    Better organization in lib/ and scripts/. Refs #8

---
+++ scripts/createBroadcasts.js
@@ -0,0 +1,33 @@
+var es = require('../es');
+var genb = require('../lib/genb');
+
+es.search({_type: 'show'}, {
+  filter: {term: {public: true}},
+  size: 9999
+}, function (err, data) {
+  if (err) throw err;
+  data.hits.hits.forEach(function (hit) {
+    //console.log(hit);
+    createBroadcasts(hit._source);
+  });
+});
+
+function createBroadcasts (show) {
+  var broadcast, broadcasts = genb(show), commands = [];
+  if (!broadcasts) return;
+
+  for (var i = 0; i < broadcasts.length; i++) {
+    broadcast = broadcasts[i];
+    commands.push({index: {_index: 'free103', _type: 'broadcast', _id: broadcast.id}});
+    commands.push(broadcast);
+
+    break;
+  }
+
+  if (commands.length) {
+    es.bulk({}, commands, function (err, data) {
+      if (err) throw err;
+      console.log(data.items[0].index);
+    });
+  } else console.log('No broadcasts for ' + show.id);
+}
