f265bbebbcc3b73b55a61cc11b042239d16f045c
Gabriel Farrell <g@gsf.io>
2015-02-21 03:06:56 +0000

    Compare password to hash with bcrypt on login. Fix #35

--- routes/login.js
+++ routes/login.js
@@ -1,3 +1,4 @@
+var bcrypt = require('bcryptjs')
 var es = require('../es')
 var scalpel = require('scalpel')
 var schemas = require('../schemas')
@@ -10,22 +11,22 @@ module.exports = stack(
     var password = req.parsedBody.password
     var token = req.parsedBody.token
     if (!(username && password) && !token) {
-      return next({status: 400, message: "Bad Request"})
+      return next({status: 400, message: 'Bad Request'})
     }
-    var filter = token ? {term: {token: token}} : {
-      and: [
-        {term: {name: username}},
-        {term: {password: password}}
-      ]
-    }
+    var filter = {term: (token ? {token: token} : {name: username})}
     es.search({_type: 'user'}, {query: {
         filtered: {filter: filter}
       }
     }, function (err, data) {
       if (err) return next(err)
-      if (!data.hits.hits.length) return next({status: 401, message: "Unauthorized"})
+      if (!data.hits.hits.length) return next({status: 401, message: 'Unauthorized'})
       var user = data.hits.hits[0]._source
-      res.send(JSON.stringify(user))
+      if (token) return res.send(JSON.stringify(user))
+      bcrypt.compare(password, user.password, function (err, auth) {
+        if (err) return next(err)
+        if (!auth) return next({status: 401, message: 'Unauthorized'})
+        res.send(JSON.stringify(user))
+      })
     })
   }
 )
