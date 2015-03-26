c5c3a82ffdeb750ff5170bb6ed3c5374cd5a2b4c
Gabriel Farrell <g@gsf.io>
2015-03-26 05:01:55 -0400

    Just require ESURL. Fix #54

--- es.js
+++ es.js
@@ -1,10 +1,14 @@
-var es = require('es')
+var es = require('es');
+var url = require('url');
+
+var u = url.parse(process.env.ESURL);
+var port = u.port || (u.protocol === 'https:' ? 443 : 80);
 
 module.exports = es({
-  _index: process.env.ESINDEX || 'free103',
+  _index: u.path.substr(1) || process.env.ESINDEX || 'free103',
   server: {
-    auth: process.env.ESAUTH || '', // set with user:pass
-    host: process.env.ESHOST || 'localhost',
-    port: process.env.ESPORT || 9200
+    auth: u.auth || process.env.ESAUTH || '', // set with user:pass
+    host: u.hostname || process.env.ESHOST || 'localhost',
+    port: port || process.env.ESPORT || 9200
   }
-})
+});
