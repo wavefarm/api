59c2275c7716bc972acbd3c5562d302bea4c4325
Gabriel Farrell <g@gsf.io>
2015-03-26 06:04:14 -0400

    And another go at ESURL. #54

--- es.js
+++ es.js
@@ -2,13 +2,12 @@ var es = require('es');
 var url = require('url');
 
 var u = url.parse(process.env.ESURL || '');
-var port = u.port || (u.protocol === 'https:' ? 443 : 80);
 
 module.exports = es({
-  _index: u.path.substr(1) || process.env.ESINDEX || 'free103',
+  _index: (u.path && u.path.substr(1)) || 'free103',
   server: {
-    auth: u.auth || process.env.ESAUTH || '', // set with user:pass
-    host: u.hostname || process.env.ESHOST || 'localhost',
-    port: port || process.env.ESPORT || 9200
+    auth: u.auth || '',
+    host: u.hostname || 'localhost',
+    port: u.port || (u.protocol === 'http:' && 80) || (u.protocol === 'https:' && 443) || 9200
   }
 });
