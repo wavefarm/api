89fedc399d461e681ef5384d98fdd60ac699a1e6
Gabriel Farrell <g@gsf.io>
2015-04-04 02:08:53 -0400

    Migration no curl

--- migrations/000-public-not-active.js
+++ migrations/000-public-not-active.js
@@ -1,17 +1,49 @@
-var fs = require('fs')
+var docStream = require('elasticsearch-doc-stream')
+var http = require('http')
+var url = require('url')
 
-require('esindexdump')({
+var d = docStream({
   url: process.env.ESURL,
-  out: fs.createWriteStream('data.ldj')
-}, function (err, doc) {
-  if (err) {
-    console.error(err)
-    process.exit(1)
+  search: {
+    query: {
+      filtered: {
+        filter: {
+          exists: {field: 'active'}
+        }
+      }
+    }
   }
+});
+
+var data = '';
+
+d.on('data', function (doc) {
   var s = doc._source
-  if (s.active) {
-    s.public = s.active
-    delete s.active
-  }
-  return doc
+  s.public = s.active
+  delete s.active
+  data += JSON.stringify({
+    index: {
+      _type: doc._type,
+      _id: doc._id
+    }
+  }) + '\n'
+  data += JSON.stringify(s) + '\n'
+});
+
+d.on('error', function (err) {
+  console.error(err)
+});
+
+d.on('end', function () {
+  //process.stdout.write(data)
+  //process.exit()
+  if (!data) return console.log('0 hits')
+
+  var opt = url.parse(process.env.ESURL + '/_bulk')
+  opt.method = 'POST'
+  http.request(opt, function (res) {
+    res.pipe(process.stdout)
+  }).on('error', function (err) {
+    console.error(err)
+  }).end(data)
 })
