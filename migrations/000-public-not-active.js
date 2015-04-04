a2d13833b4107916f6dfd8c4f447acd2c4a12cba
Gabriel Farrell <g@gsf.io>
2015-04-04 00:19:43 -0400

    Update esindexdump, doc-stream

--- migrations/000-public-not-active.js
+++ migrations/000-public-not-active.js
@@ -1,4 +1,13 @@
-require('esindexdump')(function (doc) {
+var fs = require('fs')
+
+require('esindexdump')({
+  url: process.env.ESURL,
+  out: fs.createWriteStream('data.ldj')
+}, function (err, doc) {
+  if (err) {
+    console.error(err)
+    process.exit(1)
+  }
   var s = doc._source
   if (s.active) {
     s.public = s.active
