ca95b4a0566e68119907d9ab9e34226f4368885a
Peter Berry <peter@Peters.local>
2015-05-12 13:46:58 -0700

    adds route for TA schedule

---
+++ routes/ta/schedule.js
@@ -0,0 +1,39 @@
+var es = require('../../es')
+
+module.exports = function (req, res, next) {
+  var date = req.params[0]
+  var queryBody = {
+  	size: 100,
+    filter: {
+      and: [
+        {
+          or: [
+            {term: {active: true}},
+            {term: {public: true}}
+          ]
+        },
+        {
+          range: {
+            start: {
+              from: date + 'T00:00:00',
+              to: date + 'T23:59:59'
+            }
+          }
+        },
+        {
+          term: {sites: 'transmissionarts'},
+        }
+      ]
+    },
+    sort: [{start: 'asc'}]
+  }
+  es.search({_type: 'broadcast'}, queryBody, function (err, data) {
+    if (err) return next(err)
+    res.send(JSON.stringify({
+      total: data.hits.total,
+      hits: data.hits.hits.map(function (hit) {
+        return hit._source
+      })
+    }))
+  })
+};
--- server.js
+++ server.js
@@ -68,6 +68,7 @@ http.createServer(stack(
   rut.get('/search', require('./routes/search')),
   rut.get('/schemas', require('./routes/schemas')),
   rut.get('/wgxc/schedule/*', require('./routes/wgxc/schedule')),
+  rut.get('/ta/schedule/*', require('./routes/ta/schedule')),
   rut.get('/wgxc/index-gallery', require('./routes/wgxc/index-gallery')),
   rut.post('/login', require('./routes/login')),
   rut.post('/bulk', require('./routes/bulk')),
