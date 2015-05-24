d820f8a6cd94f8365b19f4e78a18a7eb10a4880c
Gabriel Farrell <g@gsf.io>
2015-05-23 22:32:40 -0400

    Script cleanup

--- scripts/prod-migrate.sh
+++ scripts/prod-migrate.sh
@@ -1,8 +1,6 @@
 set -e
 
-curl -XDELETE $ESURL
-curl -XPUT $ESURL -d @index.json
 esindexdump $ESURLPROD > tmp.ldj
-curl -XPOST $ESURL/_bulk --data-binary @tmp.ldj
-node migrations/000-public-not-active.js
-curl -XPOST $ESURL/_bulk --data-binary @migrations/001-first-users.ldj
+curl -XDELETE $ESURLDEV
+curl -XPUT $ESURLDEV -d @index.json
+curl -XPOST $ESURLDEV/_bulk --data-binary @tmp.ldj
